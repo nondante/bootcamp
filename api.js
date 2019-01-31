cc68deadce3d6ff7577c1ce48df570a3b313267c
Pozelaite <jana.pozelaite@accenture.com>
2019-01-31 17:38:22 +0200

    REFERENCE: "Servers" DESCRIPTION: "Node.js"

---
+++ api.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+  switch (req.url) {
+    case "/":
+      res.statusCode = 200;
+      res.setHeader("Content-Type","text/plain");
+      res.end("Hello World");
+      break;
+    case "/about":
+      res.statusCode = 200;
+      res.setHeader("Content-Type","text/plain");
+      res.end("My first app");
+      break;
+    case "/contact":
+        res.statusCode = 200;
+        res.setHeader("Content-Type","text/plain");
+        res.end("Please contact me");
+        break;
+    default:
+      res.statusCode = 404;
+      res.end("Error: Page not find");
+
+  }
+
+
+});
+
+server.listen(port, hostname,()=>{
+  console.log("Listening at port 3000");
+});
